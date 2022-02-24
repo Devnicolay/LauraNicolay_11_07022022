17d13383b4c7d0dac6581bbe2c9b1eee983207ac
Laura Nicolay <devnicolay@gmail.com>
2022-02-24 17:46:58 +0100

    Modification évaluation étoiles

--- src/components/StarRating/starRating.jsx
+++ src/components/StarRating/starRating.jsx
@@ -3,12 +3,12 @@ import { FaStar } from "react-icons/fa";
 function StarRating({ rating }) {
   return (
     <div>
-      {[...Array(5)].map((star, index, i) => {
+      {[...Array(5)].map((star, i) => {
         const ratingValue = i + 1;
         return (
           <FaStar
             className="stars"
-            key={index}
+            key={i}
             values={`${ratingValue}`}
             color={ratingValue <= rating ? "#ff6060" : "#e3e3e3"}
           />
